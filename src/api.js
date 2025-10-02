53b2b33ff91d455638b47731deeed7a117a58970
Nadezhda Melnikova <naryvkinan@mail.ru>
2025-10-02 22:42:09 +0300

    Refactor: WebSocket

--- src/api.js
+++ src/api.js
@@ -1,36 +1,65 @@
 const API_KEY = '097a6402039c557407d72d752da172289d6195a6d4747bd1d95bed9ca76172dc';
 
 const tickersHandlers = new Map();
+const socket = new WebSocket(`wss://data-streamer.coindesk.com/?api_key=${API_KEY}`);
 
-// ToDo: реализовать через search params
-export const loadTicker = () => {
-    if (tickersHandlers.size === 0) {
+socket.addEventListener('message', (event) => {
+    const messageContent = JSON.parse(event.data);
+    const { TYPE: type, VALUE: newPrice, INSTRUMENT: key} = JSON.parse(event.data);
+    console.log(messageContent);
+
+    if(type !== '1101' || newPrice === undefined ) {
         return;
     }
-    [...tickersHandlers.keys()].forEach((tickerName) => {
-        const key = `${tickerName}-USD`;
-        fetch(
-            `https://data-api.coindesk.com/index/cc/v1/latest/tick?market=ccix&instruments=${key}&api_key=${API_KEY}`,
-        ).then(res => res.json())
-        .then((data) => {
-            const updatedData = data;
-            const newPrice = updatedData.Data[key]?.VALUE;
-            const hendlers = tickersHandlers.get(tickerName);
 
-            hendlers.forEach((hendler) => {
-                hendler(newPrice)
-            })
-        })
-    })
+    const ticker = key.split('-')[0];
+
+    const handlers = tickersHandlers.get(ticker) ?? [];
+    handlers.forEach(handler => handler(newPrice));
+})
+
+function sendMessageToWs(message) {
+    const stringifiedMessage = JSON.stringify(message)
+
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(stringifiedMessage);
+        return;
+    }
+    socket.addEventListener('open', () => {
+        socket.send(stringifiedMessage);
+    }, {once: true});
+}
+
+function subscribeToTickerOnWs(ticker) {
+    const message = {
+        action: 'SUB_ADD',
+        type: "1101",
+        groups: ["VALUE"],
+        subscriptions: [{"market": "cadli", "instrument": `${ticker}-USD`}]
+    }
+
+    sendMessageToWs(message);
+}
+
+function unSubscribeFromTickerOnWs(ticker) {
+    const message = {
+        action: 'SUB_REMOVE',
+        type: "1101",
+        groups: ["VALUE"],
+        subscriptions: [{"market": "cadli", "instrument": `${ticker}-USD`}]
+    }
+
+    sendMessageToWs(message);
 }
 
 export const subscribeToTicker = (ticker, collBack)  => {
     const subscribers = tickersHandlers.get(ticker) || [];
     tickersHandlers.set(ticker, [...subscribers, collBack]); // добавление функции колбека для тикера
+
+    subscribeToTickerOnWs(ticker) // подписка через WebSocket
 }
 
 export const unsubscribeFromTicker = (ticker) => {
     tickersHandlers.delete(ticker); // удаление всех коллбеков
+    unSubscribeFromTickerOnWs(ticker)
 }
-
-setInterval(loadTicker, 5000)
