cb1415c65c5147e309dbebce8bfdd762a30cede7
Nadezhda Melnikova <naryvkinan@mail.ru>
2025-10-22 15:42:47 +0300

    refactor components: ticker-card

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,16 @@
       <hr class="w-full border-t border-gray-600 my-4" />
       <div>
         <button
-            @click="page = page - 1"
-            v-if="page > 1"
-            type="button"
-            class="my-4 mr-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          @click="page = page - 1"
+          v-if="page > 1"
+          type="button"
+          class="my-4 mr-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >Назад</button>
         <button
-            @click="page = page + 1"
-            v-if="hasNextPage"
-            type="button"
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          @click="page = page + 1"
+          v-if="hasNextPage"
+          type="button"
+          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >Вперед</button>
       </div>
 
@@ -23,30 +23,30 @@
         <div>
           <label for="filter">Фильтр:</label>
           <input
-              v-model="filter"
-              type="text"
-              name="filter"
-              id="filter"
-              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-              placeholder="Например DOGE"
+            v-model="filter"
+            type="text"
+            name="filter"
+            id="filter"
+            class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            placeholder="Например DOGE"
           >
         </div>
 
         <button
-            @click="deleteAll()"
-            type="button"
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          @click="deleteAll()"
+          type="button"
+          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         ><svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="#ffffff"
-            aria-hidden="true"
+          class="h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="#ffffff"
+          aria-hidden="true"
         >
           <path
-              fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clip-rule="evenodd"
+            fill-rule="evenodd"
+            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clip-rule="evenodd"
           ></path>
         </svg>
           <span class="pl-1">Удалить все</span>
@@ -56,45 +56,15 @@
       <hr class="w-full border-t border-gray-600 my-4" />
 
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        <div
+        <ticker-card
           v-for="(t, i) in tickersInCurrentPage"
           :key="i"
           @click="select(t)"
-          :class="{
-            'border-4': selectedTicker === t,
-            'bg-red-100': t.price === '-'
-          }"
-          class="overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+          :ticker="t"
+          :selected-ticker="selectedTicker"
+          @delete-ticker="handleDelete(t)"
         >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              {{ t.name }} - USD
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">
-              {{ formatPrice(t.price) }}
-            </dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
-            @click.stop="handleDelete(t)"
-            class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-          >
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
-            >
-              <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span class="pl-1">Удалить</span>
-          </button>
-        </div>
+        </ticker-card>
       </dl>
 
       <hr class="w-full border-t border-gray-600 my-4" />
@@ -102,9 +72,9 @@
 
     <div  v-if="selectedTicker">
       <ticker-graph
-          @close-graph="selectedTicker = null"
-          :priceCurrentTicker="priceCurrentTicker"
-          :selectedTicker="selectedTicker"
+        @close-graph="selectedTicker = null"
+        :priceCurrentTicker="priceCurrentTicker"
+        :selectedTicker="selectedTicker"
       ></ticker-graph>
     </div>
   </div>
@@ -115,13 +85,15 @@
 import {subscribeToTicker, unsubscribeFromTicker} from "@/api";
 import AddTicker from "@/components/AddTicker.vue";
 import TickerGraph from "@/components/TickerGraph.vue";
+import TickerCard from "@/components/TickerCard.vue";
 
 export default {
   name: 'App',
 
   components: {
+    TickerCard,
     AddTicker,
-    TickerGraph
+    TickerGraph,
   },
 
   data() {
@@ -226,13 +198,6 @@ export default {
       } else {
         this.showErrorExist = true
       }
-    },
-
-    formatPrice(price) {
-      if(price === '-') {
-        return price
-      }
-      return price > 1 ? price.toFixed(2) : price.toPrecision(2)
     },
 
     select(ticker) {
